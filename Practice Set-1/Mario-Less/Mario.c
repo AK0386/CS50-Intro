1cb0362ddd79a628e6319efc3a9a8dcf49eb4d98
AK0386 <ananyakawatra03@gmail.com>
2024-08-22 18:47:08 +0530

    Create Mario.c

---
+++ Practice Set-1/Mario-Less/Mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    int height = 0;
+    do
+    {
+        height = get_int("What is the height?");
+    }
+    while (height < 1);
+
+    for (int i = 1; i <= height; i++)
+    {
+        int a = height - i;
+        for (int space = 1; space <= a; space++)
+        {
+            printf(" ");
+        }
+        for (int hashs = 0; hashs < i; hashs++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
